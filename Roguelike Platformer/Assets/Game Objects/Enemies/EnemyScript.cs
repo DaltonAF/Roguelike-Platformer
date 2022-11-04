9f656c120d832470ab5b1b4fba6dec2f9fbe4859
DaltonAF <115432070+DaltonAF@users.noreply.github.com>
2022-11-03 20:37:27 -0400

    cant take damage from dead enemies

--- Roguelike Platformer/Assets/Game Objects/Enemies/EnemyScript.cs
+++ Roguelike Platformer/Assets/Game Objects/Enemies/EnemyScript.cs
@@ -30,6 +30,7 @@ public class EnemyScript : MonoBehaviour
 
     public void Die(){
         //die animation
+        Physics2D.IgnoreLayerCollision(10, 11, true);
         animator.SetBool("IsDead", true);
        
         FunctionTimer.Create(Kill, 0.9f);
@@ -38,6 +39,7 @@ public class EnemyScript : MonoBehaviour
     public void Kill()
     {
         isDead = true;
+        Physics2D.IgnoreLayerCollision(10, 11, false);
         Destroy(gameObject);
     }
 }
