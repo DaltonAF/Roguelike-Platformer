1d8f46bae4d42f9ee926e15beb78f17fac03064b
HuyMa <114541770+HuyMa@users.noreply.github.com>
2022-11-12 00:04:25 -0500

    Update OptionsMenu

--- Roguelike Platformer/Assets/OptionsMenuScript.cs
+++ Roguelike Platformer/Assets/OptionsMenuScript.cs
@@ -2,14 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenuScript : MonoBehaviour
 {
-
     public AudioMixer audioMixer;
+
+    Resolution[] resolutions;
+
+    public TMPro.TMP_Dropdown resolutionDropdown;
+    
+    void Start()
+    {
+        // Sets Resolutions for Settings Dropdown
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> resOptions = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string resOption = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "hz";
+            resOptions.Add(resOption);
+
+            if(resolutions[i].width == Screen.currentResolution.width && 
+            resolutions[i].height == Screen.currentResolution.height && 
+            resolutions[i].refreshRate == Screen.currentResolution.refreshRate)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(resOptions);
+
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
 
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("MasterVolume", volume);
     }
+
+    public void SetFullscreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+
 }
