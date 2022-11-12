fac7db43b1df67b7b80cf235e8cc3508487a7d9f
HuyMa <114541770+HuyMa@users.noreply.github.com>
2022-11-11 23:02:51 -0500

    Update to Options Menu

--- Roguelike Platformer/Assets/OptionsMenuScript.cs
+++ Roguelike Platformer/Assets/OptionsMenuScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class OptionsMenu : MonoBehaviour
+public class OptionsMenuScript : MonoBehaviour
 {
 
     public AudioMixer audioMixer;
